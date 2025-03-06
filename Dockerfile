548617ac36af334fb02c47635140f2e032d89b84
hasankoroglu <aykutmervan05@gmail.com>
2025-03-06 21:09:05 +0300

    distributed systems first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+
+FROM openjdk:21-jdk-slim
+
+# Çalışma dizinini belirle
+WORKDIR /app
+
+# Maven ile build edilen JAR dosyasını container içine kopyala
+COPY target/dagitik-odevv-0.0.1-SNAPSHOT.jar /app/app.jar
+
+# Spring Boot uygulamasını başlat
+CMD ["java", "-jar", "/app/app.jar"]
---
+++ src/main/java/com/dagitik/dagitikodevv/DagitikSistemlerApplication.java
@@ -0,0 +1,13 @@
+package com.dagitik.dagitikodevv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagitikSistemlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DagitikSistemlerApplication.class, args);
+    }
+
+}
